b597c3aaf8b44d1be9ca674d9ee09c02c6d804ba
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2025-04-08 21:37:37 +0300

    response wrapper structure added

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,7 +1,14 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ParentDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.StudentDTO;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +19,34 @@ import java.util.List;
 public class SchoolController {
     //write a method for teacher and return list of teachers
     private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.parentService = parentService;
     }
 
    @GetMapping("/teachers")
     public List<TeacherDTO> getTeachers() {
         return teacherService.findAll();
    }
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> getStudents() {
+        List<StudentDTO> students = studentService.findAll();
+        return ResponseEntity
+                .ok(new ResponseWrapper("students are successfully retrieved", students));
+    }
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> getParents() {
+        List<ParentDTO> parentDTOS= parentService.findAll();
+
+        ResponseWrapper responseWrapper = new ResponseWrapper("parents are successfully retrieved", parentDTOS);
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header("Parent","Returned")
+                .body(responseWrapper);
+    }
     
 
 }
---
+++ src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,32 @@
+package com.cydeo.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseWrapper {
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.message = message;
+        this.data = data;
+        this.success = true;
+        this.code = HttpStatus.OK.value();
+
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.success = true;
+        this.code = HttpStatus.OK.value();
+    }
+
+}
